b3dcd57f6eb13af501d6bfac2ae52b27ae53f23e
Saveli Maksimchyk <svli1040@gmail.com>
2025-08-04 13:58:46 +0300

    refactor(git): Stop tracking ignored files

--- modal-rag-app/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This command is idempotent. It creates the volume only if it doesn't exist.
-modal volume create model-cache-volume
-modal volume create source-volume
-modal volume create vector-store-volume
-
-echo "Volumes 'source-volume', 'vector-store-volume', and 'model-cache-volume' are ready."
